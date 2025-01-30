8307b1263906890e32073dbbc84f52b6bcf18817
Gustavo Minetoma <gminetoma@gmail.com>
2025-01-30 23:49:23 +0900

    feat: add `Input` component

---
+++ app/components/Input.tsx
@@ -0,0 +1,28 @@
+import React, { type InputHTMLAttributes } from 'react'
+
+export interface Props extends InputHTMLAttributes<HTMLInputElement> {
+  label?: string
+  error?: string
+  inputStyle?: InputStyleValue
+}
+
+const Input = (props: Props) => {
+  const { label, error, className, inputStyle, ...inputProps } = props
+
+  return (
+    <div className="flex flex-col gap-1 mb-1">
+      {label && inputProps.id && <label htmlFor={inputProps.id}>{label}</label>}
+      <input
+        {...inputProps}
+        className={(className ? className + ' ' : '') + inputStyle}
+      />
+      <div className="relative">
+        {error && (
+          <span className="absolute text-sm text-red-500">{error}</span>
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default Input
