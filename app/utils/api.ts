db472c9474c7fd2a99b856f277a98b6bfe9914a1
Gustavo Minetoma <gminetoma@gmail.com>
2025-02-04 23:20:55 +0900

    feat: add api calls

--- app/utils/api.ts
+++ app/utils/api.ts
@@ -1,8 +1,68 @@
-import axios from '~/configs/axios'
+import type { AxiosResponse } from 'axios'
+import axiosInstance from '~/configs/axios'
 
-export const createAccount = () => {
-  axios({
-    method: 'post',
-    url: '/',
-  })
+const responseHandler = <Property extends Record<string, unknown> | undefined>(
+  response: AxiosResponse<ApiResponse<Property>>,
+) => {
+  return {
+    data: response.data.data,
+    status: response.data.status,
+    error: response.data.error,
+    response,
+  }
 }
+
+const login = async (data: Partial<LoginForm>) => {
+  const response = await axiosInstance.post<
+    ApiResponse<{ tokens: SessionTokens }>
+  >('/sessions', data)
+  return responseHandler(response)
+}
+
+const mySession = async (accessToken?: string) => {
+  const response = await axiosInstance.get<
+    ApiResponse<{ jwtPayload: JwtPayload }>
+  >(
+    '/sessions/my',
+    accessToken
+      ? { headers: { Authorization: `bearer ${accessToken}` } }
+      : undefined,
+  )
+  return responseHandler(response)
+}
+
+const refreshSession = async (refreshToken?: string) => {
+  const response = await axiosInstance.post<
+    ApiResponse<{ tokens: SessionTokens }>
+  >(
+    '/sessions/my/refresh',
+    {},
+    refreshToken
+      ? { headers: { Authorization: `bearer ${refreshToken}` } }
+      : undefined,
+  )
+  return responseHandler(response)
+}
+
+const logout = async () => {
+  const response = await axiosInstance.delete<ApiResponse>('/sessions/my')
+  return responseHandler(response)
+}
+
+const createAccount = async (createAccountInput: CreateAccountForm) => {
+  const response = await axiosInstance.post<ApiResponse<{ account: Account }>>(
+    '/accounts',
+    createAccountInput,
+  )
+  return responseHandler(response)
+}
+
+const apiCall = {
+  createAccount,
+  mySession,
+  login,
+  logout,
+  refreshSession,
+}
+
+export default apiCall
