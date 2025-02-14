43e368419ae19053bca4174059e9649d776589bf
Gustavo Minetoma <gminetoma@gmail.com>
2025-02-14 23:24:58 +0900

    feat: add profile form validation

--- app/components/ProfileForm.tsx
+++ app/components/ProfileForm.tsx
@@ -1,15 +1,42 @@
 import React from 'react'
 import Button from './Button'
 import Input from './Input'
+import { useForm, type SubmitHandler } from 'react-hook-form'
 
-const ProfileForm = () => {
+type Props = {
+  onValid: SubmitHandler<ProfileForm>
+}
+
+const ProfileForm = ({ onValid }: Props) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ProfileForm>({ defaultValues: {} })
+
+  const handleOnValid = (data: ProfileForm) => {
+    data.formId = 'PROFILE_FORM'
+    onValid(data)
+  }
+
   return (
-    <form className="flex flex-col gap-5">
-      <Input label="Full Name" name="full_name" id="full_name" />
-      <Input label="Job Title" name="job_title" id="job_title" />
-      <Input label="Email" name="email" id="email" />
-      <Input label="Phone" name="phone" id="phone" />
-      <Input label="Address" name="address" id="address" />
+    <form
+      className="flex flex-col gap-5"
+      onSubmit={handleSubmit(handleOnValid)}
+    >
+      <Input
+        label="Full Name"
+        id="fullName"
+        {...register('fullName', {
+          required: { value: true, message: 'Required' },
+        })}
+        error={errors.fullName?.message}
+        inputStyle="primary"
+      />
+      <Input label="Job Title" id="jobTitle" {...register('jobTitle')} />
+      <Input label="Email" id="email" {...register('email')} />
+      <Input label="Phone" id="phone" {...register('phone')} />
+      <Input label="Address" id="address" {...register('address')} />
       <Button buttonStyle="primary" className="w-full">
         Save
       </Button>
