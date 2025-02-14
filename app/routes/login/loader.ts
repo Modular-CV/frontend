6a81d5e04b70c34a84fccd1e8ca36d7376fa2d7a
Gustavo Minetoma <gminetoma@gmail.com>
2025-02-14 23:23:20 +0900

    fix: set cookie on login loader

--- app/routes/login/loader.ts
+++ app/routes/login/loader.ts
@@ -2,9 +2,12 @@ import { redirect, type LoaderFunctionArgs } from 'react-router'
 import { getTokens } from '~/utils'
 
 const loader = async ({ request }: LoaderFunctionArgs) => {
-  const { error } = await getTokens(request.headers)
+  const { error, setCookie } = await getTokens(request.headers)
 
-  if (!error) return redirect('/')
+  if (!error)
+    return redirect('/', {
+      headers: setCookie?.map((cookie) => ['set-cookie', cookie]),
+    })
 }
 
 export default loader
